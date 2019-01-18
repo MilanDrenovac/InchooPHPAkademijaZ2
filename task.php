9ea74d1fdf0071bd52f4950472fcdd51bb97300b
Milan Drenovac <milandrenovac@gmail.com>
2019-01-18 12:13:42 -0800

    Sorted and Printed the bold number

--- task.php
+++ task.php
@@ -4,5 +4,24 @@
 $num = $_POST['numbers'];
 
 $seperated = explode (",", $num);
-print_r($seperated);
+//print_r($seperated);
 
+foreach($seperated as $n){
+    if ($n % 2 == 0){
+        $even[] = $n;
+    }
+}
+
+sort($even);
+print_r($even);
+echo '<hr />';
+print_r($seperated);
+$mean = array_sum($seperated) / count($seperated);
+echo $mean;
+
+foreach($even as $bold){
+    if ($bold > $mean) {
+        echo '<b>', $bold, '</b>';
+        break;
+    }
+}
